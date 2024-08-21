f8d9514f7896bb23734333ab49049b909ffeae7e
Cheriefox <164426395+Cheriefox@users.noreply.github.com>
2024-08-21 18:56:45 -0300

    Update index.php
    actualizo el index haciendo dinamico la subida de productos

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
+<?php
+
+require 'config/database.php';
+$db = new Database();
+$con = $db->conectar();
+
+$sql= $con->prepare("SELECT id, nombre, precio FROM productos WHERE activo=1");
+$sql->execute();
+$resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,7 +36,7 @@
         <span class="navbar-toggler-icon"></span>
       </button>jijo
 
-      <div class="collaspse navbar-collapse" id="navbarHeader">
+      <div class="collapse navbar-collapse" id="navbarHeader">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
             <a href="#" class="nav-link active">Catalogo</a>
@@ -40,12 +53,22 @@
 <main>
   <div class="container">
   <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    <?php foreach($resultado as $row) { ?>
+
         <div class="col">
           <div class="card shadow-sm">
-            <img src="producto.png">
+            <?php
+            
+            $id = $row['id'];
+            $imagen = "images/producto/".$id."/producto.png";
+            if(!file_exists($imagen)){
+              $imagen = "images/no-photo.jpg";
+            }
+            ?>
+            <img src="<?php echo $imagen ?>">
             <div class="card-body">
-              <h5 class="card-title">Producto de muestra #1</h5>
-              <p class="card-text">$599.99</p>
+              <h5 class="card-title"><?php echo $row['nombre'];?></h5>
+              <p class="card-text"><?php echo number_format($row['precio'],2,'.',',');?></p>
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
                  <a href="" class="btn btn-primary">Detalles</a>
@@ -55,36 +78,7 @@
             </div>
           </div>
         </div>
-        <div class="col">
-          <div class="card shadow-sm">
-            <img src="producto.png">
-            <div class="card-body">
-              <h5 class="card-title">Producto de muestra #1</h5>
-              <p class="card-text">$599.99</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                 <a href="" class="btn btn-primary">Detalles</a>
-                </div>
-                <a href="" class="btn btn-success">Agregar</a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col">
-          <div class="card shadow-sm">
-            <img src="producto.png">
-            <div class="card-body">
-              <h5 class="card-title">Producto de muestra #1</h5>
-              <p class="card-text">$599.99</p>
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                 <a href="" class="btn btn-primary">Detalles</a>
-                </div>
-                <a href="" class="btn btn-success">Agregar</a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <?php  } ?>
   </div>
 </main>
 
